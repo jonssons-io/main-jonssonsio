487dd908e5dfc644a7a6ea92f676637d79315dc0
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2021-04-06 18:55:49 +0200

    Layout, colours and animations created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main id="app" class="fill-height fill-width d-flex">
+    <div class="fill-height left"></div>
+    <div class="fill-height right d-flex flex-col align-center justify-center">
+      <h1 class="d-flex">
+        Coming s<span class="first-double-o firstbounce">o</span
+        ><span class="second-double-o secondbounce">o</span>
+        <div class="spacer"></div>
+        n
+      </h1>
+      <h3 class="mt-05">We're in the process of building our website.</h3>
+      <div class="section-spacer"></div>
+      <hr class="divider" />
+      <div class="section-spacer"></div>
+      <section class="contact ms-3">
+        <p>
+          In the meantime, we are happy to connect through our other socials.
+        </p>
+        <div class="section-spacer"></div>
+        <div class="profile-bubbles d-flex justify-space align-center">
+          <img
+            class="bubble"
+            :class="[visibility.gabrielle ? 'active' : '']"
+            src="./assets/img/GabCrimsonG.png"
+            alt="Image of Gabrielle"
+            @click="handleSocialMenu('gabrielle')"
+          />
+
+          <img
+            class="bubble"
+            :class="[visibility.axel ? 'active' : '']"
+            src="./assets/img/GabCrimsonG.png"
+            alt="Image of Gabrielle"
+            @click="handleSocialMenu('axel')"
+          />
+        </div>
+        <div class="section-spacer"></div>
+        <div
+          v-if="!visibility.gabrielle && !visibility.axel"
+          class="section-spacer"
+        ></div>
+        <div
+          v-if="visibility.gabrielle"
+          class="contact-gabrielle d-flex justify-start"
+        >
+          <font-awesome-icon
+            class="fai fai-gabrielle ms-1 mt-05"
+            :icon="['fab', 'github-square']"
+          />
+          <font-awesome-icon
+            class="fai fai-gabrielle ms-1 mt-05"
+            :icon="['fab', 'linkedin']"
+          />
+          <font-awesome-icon class="fai fai-gabrielle ms-1 mt-05" icon="at" />
+        </div>
+        <div v-if="visibility.axel" class="contact-axel d-flex justify-end">
+          <font-awesome-icon
+            class="fai fai-axel ms-1 mt-05"
+            :icon="['fab', 'github-square']"
+          />
+          <font-awesome-icon
+            class="fai fai-axel ms-1 mt-05"
+            :icon="['fab', 'linkedin']"
+          />
+          <font-awesome-icon class="fai fai-axel ms-1 mt-05" icon="at" />
+        </div>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      visibility: {
+        axel: false,
+        gabrielle: true,
+      },
+    };
+  },
+  methods: {
+    handleSocialMenu(person) {
+      if (person == "gabrielle") {
+        this.visibility.axel = false;
+        this.visibility.gabrielle = !this.visibility.gabrielle;
+      }
+      if (person == "axel") {
+        this.visibility.gabrielle = false;
+        this.visibility.axel = !this.visibility.axel;
+      }
+    },
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "./../public/stylesheets/styles.css";
+
+library.add(faGithubSquare, faLinkedin, faAt);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
